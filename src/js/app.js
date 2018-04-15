2668556f690a9603bcf7f248a31c5806a1080f68
tripellington <zackellington@gmail.com>
2018-04-15 10:31:22 -0400

    Addition of iterator for skills JSON

--- src/js/app.js
+++ src/js/app.js
@@ -3,8 +3,14 @@ require('bulma');
 
 const pages = require('./pages.js');
 const $ = require('jquery');
+const skills = require('../data/skills.json');
 
-$.each(pages, (key, value) => {
-  $('body').append('<h1>' + pages[key].name + '</h1>');
+$.each(skills, (key, value) => {
+  $('body').append('<section class="section"><div class="container">');
+  $('body').append(`<h1 class="title">${skills[key].grouping}</h1>`);
+  $('body').append(`<h2 class="subtitle">${skills[key].skills}</h2>`);
+  $('body').append(`<p>me too</p>`);
+  $('body').append('</div></section>');
+
+ 
 });
-
